a050684e48f20a95f0447a273f4abef664726ec3
RGV <ricardo.guzman.velasco@outlook.com>
2024-03-11 18:13:11 +0100

    feat: keep the screen awaken

---
+++ Assets/KeepTheScreenAwaken.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class KeepTheScreenAwaken : MonoBehaviour
+    {
+        void Awake() => Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+}
